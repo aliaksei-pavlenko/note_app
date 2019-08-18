309b15ae6c01056748844c375157ad3e7d838d7a
Aliaksei <aliakseipavlenko@gmail.com>
2019-08-18 18:00:22 +0300

    Initial commit

---
+++ source/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let App = () => {
+    return (
+        <div>hello react!</div>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'))
