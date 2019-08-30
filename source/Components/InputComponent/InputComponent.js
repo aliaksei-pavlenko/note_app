63aa16a8b0883830476202d638e0635783f17e6f
Aliaksei <aliakseipavlenko@gmail.com>
2019-08-30 21:28:31 +0300

    creating note object logic move to back-end

--- source/Components/InputComponent/InputComponent.js
+++ source/Components/InputComponent/InputComponent.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './style.css';
 import onInputCallback from './onInputCallback';
-import makeNoteObject from '../../functions/makeNoteObject/makeNoteObject';
 
-let InputComponent = () => {
-
-    
+let InputComponent = (props) => {
     return (
-        <div>
+        <div className={'InputComponent'}>
             <div contentEditable className={'editDiv'} onInput={onInputCallback}></div>
             <div className={'editTags'}></div>
-            <button>add note</button>
+            <div className={'addNote-button'} onClick={()=>{props.addNote(document.getElementsByClassName('editDiv')[0].innerText);
+                                document.getElementsByClassName('editDiv')[0].innerText = '';
+                                document.getElementsByClassName('editTags')[0].innerText = '';
+                                }}></div>
         </div>
     )
 }
