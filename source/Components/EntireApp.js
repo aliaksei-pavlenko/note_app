bb7bb3cbaea8732a8842c5e23595cdd8ee8ec91f
Aliaksei <aliakseipavlenko@gmail.com>
2019-08-30 21:34:38 +0300

    add components in main components add CRUD methods

--- source/Components/EntireApp.js
+++ source/Components/EntireApp.js
@@ -1,12 +1,91 @@
 import React from 'react';
+//Components
 import InputComponent from './InputComponent/InputComponent';
+import SingleNoteComponent from './SingleNoteComponent/SingleNoteComponent';
+import FilterNotesComponent from './FilterNotesComponent/FilterNotesComponent';
+// Functions
 
-let EntireApp = () => {
-    return (
-        <div>
-            <InputComponent/>
-        </div>
-    )
+
+
+
+class EntireApp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            notes: []
+        }
+    }
+
+    // Create
+    addNote = (string) => {
+
+        let fetchOptions = {
+            method: 'post',
+            body: string
+        }
+        fetch('http://localhost:9000', fetchOptions)
+            .then(res => res.json())
+            .then(obj => this.setState(obj))
+    }
+
+    // Update
+    updateNote = (id,newtext) => {
+        let fetchOptions = {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id,newtext})
+        }
+        fetch('http://localhost:9000', fetchOptions)
+            .then(res => res.json())
+            .then(obj => {this.setState(obj,()=>{this.whichNoteWillEdit('a')})})
+    }
+    // Read
+    getNotes(){
+        fetch('http://localhost:9000')
+            .then(res => res.json())
+            .then(obj => this.setState(obj))
+    }
+    // Delete
+    deleteNote = (id) =>{
+        let fetchOptions = {
+            method: 'delete',
+            body: id
+        }
+        fetch('http://localhost:9000', fetchOptions)
+            .then(res => res.json())
+            .then(obj => this.setState(obj))
+    }
+
+    componentDidMount(){
+       this.getNotes();
+    }
+
+    whichNoteWillEdit = (id) => {
+        this.setState({editNoteId:id})
+    }
+ 
+    render () {
+        return (
+            <div className={'EntireApp'}>
+                <InputComponent addNote={this.addNote}/>
+                <FilterNotesComponent/>
+                {this.state.notes.map((note)=>{
+                    return(
+                        <SingleNoteComponent
+                            key = {note.id}
+                            note = {note}
+                            whichNoteWillEdit = {this.whichNoteWillEdit}
+                            editNoteId = {this.state.editNoteId}
+                            deleteNote = {this.deleteNote}
+                            updateNote = {this.updateNote}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
 export default EntireApp;
